92a2689ce5f7207ec0d0117e14a2a4ae4eef43a2
Sunil Gopinath <Sunil.Gopinath@admins-MBP-2.lan>
2016-09-13 00:17:10 +1000

    Changed routing to vestigo...lets see

--- gozleme.go
+++ gozleme.go
@@ -1,45 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"gopkg.in/husobee/vestigo.v1"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func returnArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "returns a specific article")
-	fmt.Println("Endpoint Hit: returnArticle")
-}
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "All Articles")
-	fmt.Println("Endpoint Hit: returnAllArticles")
-}
-
-func addArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Adds an article to list of articles")
-	fmt.Println("Endpoint Hit: addArticle")
-}
-
-func delArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "deletes a specific article")
-	fmt.Println("Endpoint Hit: delArticle")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/all", returnAllArticles)
-	http.HandleFunc("/single", returnArticle)
-	http.HandleFunc("/delete", delArticle)
-	http.HandleFunc("/add", addArticle)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
 func main() {
-	handleRequests()
+	// create a new vestigo router
+	router := vestigo.NewRouter()
+
+	// Define a GET /welcome route, and
+	// specify the standard http.HandlerFunc
+	// to run from that route
+	router.Get("/welcome", GetWelcomeHandler)
+
+	// Define a POST /welcome/:name where :name
+	// is a URL parameter that will be available
+	// anywhere you have access to the http.Request
+	router.Post("/welcome/:name", PostWelcomeHandler)
+
+	// the router implements http.Handler
+	// so you just need to feed it into
+	// http.ListenAndServe, or any other handler
+	// that takes handlers such as common golang
+	// middlewares
+	log.Fatal(http.ListenAndServe(":1234", router))
+}
+
+// PostWelcomeHandler - Is an Implementation of http.HandlerFunc
+func PostWelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	name := vestigo.Param(r, "name") // url params live in the request
+	w.WriteHeader(200)
+	w.Write([]byte("welcome " + name + "!"))
+}
+
+// GetWelcomeHandler - Is an Implementation of http.HandlerFunc
+func GetWelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("welcome!"))
 }
